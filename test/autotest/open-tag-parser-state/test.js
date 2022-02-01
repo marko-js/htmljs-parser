1859872819136335ee906e77c0ed1d162e239db4
Dylan Piercey <dpiercey@ebay.com>
2022-01-31 20:03:02 -0700

    refactor: cleanup some apis, merge parsers

--- test/autotest/open-tag-parser-state/test.js
+++ test/autotest/open-tag-parser-state/test.js
@@ -1,21 +1,17 @@
 exports.openTagHandlers = {
-    html: function(event) {
-        this.enterHtmlContentState();
-    },
+  javascript: function (event) {
+    this.enterJsContentState();
+  },
 
-    javascript: function(event) {
-        this.enterJsContentState();
-    },
+  css: function (event) {
+    this.enterCssContentState();
+  },
 
-    css: function(event) {
-        this.enterCssContentState();
-    },
+  text: function (event) {
+    this.enterStaticTextContentState();
+  },
 
-    text: function(event) {
-        this.enterStaticTextContentState();
-    },
-
-    parsedtext: function(event) {
-        this.enterParsedTextContentState();
-    }
+  parsedtext: function (event) {
+    this.enterParsedTextContentState();
+  },
 };
