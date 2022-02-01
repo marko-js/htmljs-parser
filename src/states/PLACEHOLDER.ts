f971c120716aca9323d0d065e4e16ecee082aefd
Dylan Piercey <dpiercey@ebay.com>
2022-01-31 20:50:30 -0700

    chore: delete some code

--- src/states/PLACEHOLDER.ts
+++ src/states/PLACEHOLDER.ts
@@ -31,21 +31,8 @@ export const PLACEHOLDER: StateDefinition<PlaceholderPart> = {
     this.notifiers.notifyPlaceholder(placeholder);
   },
 
-  return(childState, childPart, placeholder) {
-    switch (childState) {
-      case STATE.EXPRESSION: {
-        placeholder.value = cloneValue(childPart as ValuePart);
-        this.exitState("}");
-        break;
-      }
-    }
-  },
-
-  eol(str) {
-    throw new Error("Illegal state. EOL not expected");
-  },
-
-  eof() {
-    throw new Error("Illegal state. EOF not expected");
+  return(_, childPart, placeholder) {
+    placeholder.value = cloneValue(childPart as ValuePart);
+    this.exitState("}");
   },
 };
