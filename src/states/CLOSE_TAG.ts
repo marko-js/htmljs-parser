0e6d365b3d150d055d009c741e1e7d01be7878f1
Dylan Piercey <dpiercey@ebay.com>
2022-02-09 13:47:27 -0700

    refactor: update close tag to track positions only

--- src/states/CLOSE_TAG.ts
+++ src/states/CLOSE_TAG.ts
@@ -3,27 +3,18 @@ import {
   STATE,
   Part,
   StateDefinition,
-  ValuePart,
   Parser,
   peek,
+  Pos,
 } from "../internal";
 
 export interface CloseTagPart extends Part {
-  tagName: ValuePart;
+  tagName: Pos;
 }
 
 // We enter STATE.CLOSE_TAG after we see "</"
 export const CLOSE_TAG: StateDefinition<CloseTagPart> = {
   name: "CLOSE_TAG",
-
-  enter(closeTag) {
-    const tagNamePos = closeTag.pos + 2;
-    closeTag.tagName = {
-      value: "",
-      pos: tagNamePos,
-      endPos: tagNamePos,
-    } as ValuePart;
-  },
 
   eof(closeTag) {
     this.notifyError(
@@ -33,14 +24,11 @@ export const CLOSE_TAG: StateDefinition<CloseTagPart> = {
     );
   },
 
-  char(ch, code, closeTag) {
+  char(_, code, closeTag) {
     if (code === CODE.CLOSE_ANGLE_BRACKET) {
       this.exitState(">");
-      this.closeTag(closeTag.pos, closeTag.endPos, closeTag.tagName);
+      this.closeTag(closeTag);
       this.enterState(STATE.HTML_CONTENT);
-    } else {
-      closeTag.tagName.value += ch;
-      closeTag.tagName.endPos++;
     }
   },
 };
@@ -48,6 +36,7 @@ export const CLOSE_TAG: StateDefinition<CloseTagPart> = {
 export function checkForClosingTag(parser: Parser) {
   // Look ahead to see if we found the closing tag that will
   // take us out of the EXPRESSION state...
+  // TODO: instead of substringing the tagName, we should string compare two ranges in the source text.
   const match =
     parser.lookAheadFor("/>") ||
     parser.lookAheadFor(
@@ -62,11 +51,7 @@ export function checkForClosingTag(parser: Parser) {
     const pos = parser.pos;
     const endPos = parser.skip(match.length + 1);
     parser.endText();
-    parser.closeTag(pos, endPos, {
-      value: match.slice(1, -1),
-      pos: pos + 2,
-      endPos: endPos - 1,
-    } as ValuePart);
+    parser.closeTag({ pos, endPos });
     parser.enterState(STATE.HTML_CONTENT);
     parser.forward = false;
     return true;
