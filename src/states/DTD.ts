58ae85f4ac4d6f702d76310438dd6fb584ef3403
Dylan Piercey <dpiercey@ebay.com>
2022-02-09 10:18:56 -0700

    refactor: update doctype to track positions only

--- src/states/DTD.ts
+++ src/states/DTD.ts
@@ -1,21 +1,23 @@
-import { CODE, StateDefinition, ValuePart } from "../internal";
+import { CODE, StateDefinition } from "../internal";
 
 // We enter STATE.DTD after we encounter a "<!" while in the STATE.HTML_CONTENT.
 // We leave STATE.DTD if we see a ">".
-export const DTD: StateDefinition<ValuePart> = {
+export const DTD: StateDefinition = {
   name: "DTD",
 
-  enter(documentType) {
+  enter() {
     this.endText();
-    documentType.value = "";
   },
 
   exit(documentType) {
-    this.notifiers.notifyDocumentType(documentType);
-  },
-
-  eol(str, documentType) {
-    documentType.value += str;
+    this.notifiers.notifyDocumentType({
+      pos: documentType.pos,
+      endPos: documentType.endPos,
+      value: {
+        pos: documentType.pos + 2, // strip <!
+        endPos: documentType.endPos - 1, // strip >
+      },
+    });
   },
 
   eof(documentType) {
@@ -26,11 +28,9 @@ export const DTD: StateDefinition<ValuePart> = {
     );
   },
 
-  char(ch, code, documentType) {
+  char(_, code) {
     if (code === CODE.CLOSE_ANGLE_BRACKET) {
       this.exitState(">");
-    } else {
-      documentType.value += ch;
     }
   },
 };
