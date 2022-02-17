ecf975e93ea3afb2ff63ceaf42d9ae2bc88e1339
Dylan Piercey <dpiercey@ebay.com>
2022-02-17 14:43:09 -0700

    refactor: avoid using substring for open tag

--- src/internal.ts
+++ src/internal.ts
@@ -1,7 +1,6 @@
 export * from "./util/constants";
 export * from "./util/util";
 export * as operators from "./util/operators";
-export * as htmlTags from "./util/html-tags";
 export * from "./util/notify-util";
 export * from "./core/Parser";
 export * as STATE from "./states";
