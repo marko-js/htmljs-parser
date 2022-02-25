f4cea423d70e02149ad50cfa7f796613bad12acd
Dylan Piercey <dpiercey@ebay.com>
2022-02-25 09:15:20 -0700

    feat: expose legacy parser API for testing with Marko 5

--- src/internal.ts
+++ src/internal.ts
@@ -2,4 +2,5 @@ export * from "./util/constants";
 export * from "./util/util";
 export * as operators from "./util/operators";
 export * from "./core/Parser";
+export * from "./core/TempParser";
 export * as STATE from "./states";
