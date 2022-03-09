eae21f2b0ad9378910237841415d979c7d65be82
Dylan Piercey <dpiercey@ebay.com>
2022-03-09 09:26:16 -0700

    fix: tag name source positions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { Parser, TempParser } from "./internal";
 
+export type { Events, Range, ValueRange, TemplateRange } from "./internal";
+
+export { EventTypes, OpenTagEnding } from "./internal";
+
 export function createParser(text: string, filename: string) {
   return new Parser(text, filename);
 }
