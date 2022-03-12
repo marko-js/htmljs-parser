c3e297e8ec9d57da5914b7cc75670f0847e5c5ea
Dylan Piercey <dpiercey@ebay.com>
2022-03-12 15:25:00 -0700

    chore: remove unreferenced operators helper

--- src/internal.ts
+++ src/internal.ts
@@ -1,6 +1,4 @@
 export * from "./util/constants";
 export * from "./util/util";
-export * as operators from "./util/operators";
 export * from "./core/Parser";
-export * from "./core/TempParser";
 export * as STATE from "./states";
