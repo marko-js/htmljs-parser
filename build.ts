1f85c0226d211ce3d29d86e8196fa3e0dbb3be80
Dylan Piercey <dpiercey@ebay.com>
2022-03-16 15:45:58 -0700

    chore: update bench script to use dist

--- build.ts
+++ build.ts
@@ -1,32 +1,12 @@
-import fs from "fs";
-import path from "path";
-import glob from "fast-glob";
 import { build, BuildOptions } from "esbuild";
 
 (async () => {
-  const entryPoints = [];
-  const srcdir = path.resolve("src");
-  const outdir = path.resolve("dist");
-  const files = glob.stream(["**", "!*.d.ts", "!**/__tests__"], {
-    cwd: srcdir,
-  }) as AsyncIterable<string>;
-
-  for await (const file of files) {
-    if (path.extname(file) === ".ts") {
-      entryPoints.push(path.resolve(srcdir, file));
-    } else {
-      const outfile = path.join(outdir, file);
-      await fs.promises.mkdir(path.dirname(outfile), { recursive: true });
-      await fs.promises.copyFile(path.join(srcdir, file), outfile);
-    }
-  }
-
   const opts: BuildOptions = {
-    outdir,
-    entryPoints,
-    outbase: srcdir,
+    bundle: true,
+    outdir: "dist",
     platform: "node",
     target: ["node14"],
+    entryPoints: ["src/index.ts"],
   };
 
   await Promise.all([
@@ -37,23 +17,7 @@ import { build, BuildOptions } from "esbuild";
     build({
       ...opts,
       format: "esm",
-      bundle: true,
-      splitting: true,
       outExtension: { ".js": ".mjs" },
-      plugins: [
-        {
-          name: "external-modules",
-          setup(build) {
-            build.onResolve(
-              { filter: /^[^./]|^\.[^./]|^\.\.[^/]/ },
-              ({ path }) => ({
-                path,
-                external: true,
-              })
-            );
-          },
-        },
-      ],
     }),
   ]);
 })();
