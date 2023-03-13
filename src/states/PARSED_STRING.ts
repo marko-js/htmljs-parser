fe985307a2112ba48c1317a16481c321ba256619
Dylan Piercey <dpiercey@ebay.com>
2023-03-13 14:37:17 -0700

    fix: parsed text string literals

---
+++ src/states/PARSED_STRING.ts
@@ -0,0 +1,42 @@
+import { CODE, ErrorCode, STATE, StateDefinition, Meta } from "../internal";
+
+interface ParsedStringMeta extends Meta {
+  quoteCharCode: number;
+}
+
+export const PARSED_STRING: StateDefinition<ParsedStringMeta> = {
+  name: "PARSED_STRING",
+
+  enter(parent, start) {
+    return {
+      state: PARSED_STRING,
+      parent,
+      start,
+      end: start,
+      quoteCharCode: CODE.DOUBLE_QUOTE,
+    } as ParsedStringMeta;
+  },
+
+  exit() {},
+
+  char(code, str) {
+    if (code === str.quoteCharCode) {
+      this.pos++; // skip end quote
+      this.exitState();
+    } else if (!STATE.checkForPlaceholder(this, code)) {
+      this.startText();
+    }
+  },
+
+  eof(str) {
+    this.emitError(
+      str,
+      ErrorCode.INVALID_TEMPLATE_STRING,
+      "EOF reached while parsing string expression"
+    );
+  },
+
+  eol() {},
+
+  return() {},
+};
